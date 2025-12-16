c84dd4a4ab63c50b18d2fbc5c531fb011679c01a
Anthony Xavier <anthonyltx12@gmail.com>
2025-12-16 10:38:55 -0300

    Hero feito e add as imagens no projeto

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "./index.css";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <NavBar />
-      </div>
-    </>
+    <div>
+      <NavBar />
+      <Hero />
+    </div>
   );
 }
 
